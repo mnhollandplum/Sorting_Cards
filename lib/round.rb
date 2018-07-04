560af80ee050ea67072a67b5c5cc18af53e39bd1
Nikki Holland-Plum <mnhollandplum@gmail.com>
2018-07-04 14:27:11 -0600

    adds test for deck in round

--- lib/round.rb
+++ lib/round.rb
@@ -1,6 +1,12 @@
 require 'pry'
 #create class
 class Round
+#allow test to read variables
+attr_reader :deck
+#initialize method and create instance variables for...?
+  def initialize(deck)
+    @deck = deck
+  end
 
 
 
