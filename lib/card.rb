1aa7ed40a673d7fd88d7c790146b41b32d78de7d
Nikki Holland-Plum <mnhollandplum@gmail.com>
2018-07-04 22:05:02 -0600

    assigns card values for card value and suit to organize for sorting

--- lib/card.rb
+++ lib/card.rb
@@ -1,8 +1,9 @@
+require 'pry'
 class Card
 #allow other files to read the value and suit
  attr_reader :value, :suit
  #create instance variables for value and suit
-  def initialize (value, suit)
+  def initialize(value, suit)
     @value = value
     @suit = suit
   end
@@ -14,4 +15,12 @@ class Card
   def suit
     @suit
   end
+#change the suits into numerical values using keys (card suit) and values (numerical value assigned) to determine where they are in the sorted deck.
+  def card_values
+    assign_card_values = {"2" => 20, "3" => 30, "4" => 40, "5" => 50, "6" => 60, "7" => 70, "8" => 80, "9" => 90, "10" => 100, "Jack" => 110, "Queen" => 120, "King" => 130, "Ace" => 140, "Clubs" => 1, "Diamonds" => 2, "Hearts" => 3, "Spades" => 4}
+    card_values = assign_card_value[@value] + assign_card_value[@suit]
+    card_values
+
+  end
+
 end
